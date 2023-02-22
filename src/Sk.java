dc77f6db3304618433e94268af1e98fd74676e58
srinivask44 <Srinivaskondapaneni9@gmail.com>
2023-02-21 19:24:58 -0600

    Sample2

---
+++ src/Sk.java
@@ -0,0 +1,19 @@
+/*public class Sk {
+}*/
+
+
+import java.util.Scanner;
+public class Sk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println(ch + " is a vowel.");
+        } else {
+            System.out.println(ch + " is a consonant.");
+        }
+    }
+}
