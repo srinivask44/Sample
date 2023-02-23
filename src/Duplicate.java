72f9f603179532733ea29ef8a76d6b6cc3771eb3
srinivask44 <Srinivaskondapaneni9@gmail.com>
2023-02-22 18:59:05 -0600

    WaterBottle

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -1,18 +1,22 @@
 /*public class Duplicate {
 }*/
+//main class
 public class Duplicate {
+    //main method
     public static void main(String[] args) {
-        String str = "Hello, World!";
-        findDuplicateCharacters(str);
+        String Drink = "water bottle";
+        findDuplicateCharacters(Drink);
     }
-    public static void findDuplicateCharacters(String str) {
-
-        char[] chars = str.toCharArray();
+    //method 1
+    public static void findDuplicateCharacters(String Drink) {
+//creating the sequence of characters
+        char[] chars = Drink.toCharArray();
+//used to count the array
         int[] charCount = new int[256];
         for (char c : chars) {
             charCount[c]++;
         }
-        System.out.println("Duplicate characters in the String \"" + str + "\" are:");
+        System.out.println("Duplicate characters in the String \"" + Drink + "\" is:");
         for (int i = 0; i < charCount.length; i++) {
             if (charCount[i] > 1) {
                 System.out.println((char) i + " (count = " + charCount[i] + ")");
